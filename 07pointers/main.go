44e2df496f3d4853ceee2e415cfbc5ce38413a57
Anmol Anand <anmolanand2k15@gmail.com>
2022-08-10 15:46:14 +0530

    Added Pointers :

---
+++ 07pointers/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Pointers")
+
+	// var ptr *int
+	// fmt.Println(ptr)
+
+	myNumber := 23
+
+	var ptr = &myNumber
+
+	fmt.Println(ptr)
+	fmt.Println(*ptr)
+
+	*ptr = *ptr * 2
+	fmt.Println(myNumber)
+}
+
+// Reference ->  &
+// Declaring -> *
