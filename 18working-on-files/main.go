af56c4404d83ba438bdb1bc4c572b0627c69e476
Anmol Anand <anmolanand2k15@gmail.com>
2022-10-21 16:35:45 +0530

    Added File-Handling

--- 18working-on-files/main.go
+++ 18working-on-files/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome To Files in Golang")
+
+	content := "To Be Pushed Into File"
+
+	file, err := os.Create("./test.txt")
+	checkNilErr(err)
+
+	length, err := io.WriteString(file, content)
+	checkNilErr(err)
+
+	fmt.Println(length)
+	defer file.Close()
+	readFile("./test.txt")
+}
+
+func readFile(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+
+	fmt.Println(string(data))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
